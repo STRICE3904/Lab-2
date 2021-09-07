9c8aa1c3bf60b0d26affdbe934a2bc28dfd55c79
STRICE3904 <trice4@tritons.iowacentral.edu>
2021-09-07 10:55:00 -0500

    Finnal Copy

--- Lab 2/Lab 2 part 1.cpp
+++ Lab 2/Lab 2 part 1.cpp
@@ -22,41 +22,46 @@ int main()
 	const string PHY212 = "Classical Physiscs 302";
 	const string MAT180 = "Engineering Problems 109";
 	const string ThSp = "   ";
+	//Three spaces
 	const string FoSp = "    ";
+	//Four spaces
 	const string FSp = "     ";
+	//Five spaces
 	const string fct = "8:00 ";
 	const string sct = "8:20 ";
 	const string tct = "10:00";
 	const string foct = "2:00 ";
 	const string po = "******Part One******";
+	//Header for the first part
 	const string pt = "******Part Two******";
+	//Header for the second part
 	const string STAROne = "*   *   *   *   ";
 	const string StarTwo = "  *   *   *   *";
 	cout << po << endl;
-	/*
-	Part 1 class schedule
-	*/
+	//header for part 1
 	cout << M << ThSp << fct << ThSp << MAT216 << endl;
 	cout << M << ThSp << tct << ThSp << CIS162 << endl;
 	cout << M << ThSp << foct << ThSp << PHY212 << endl;
 	cout << FSp << endl;
+	//Used for gaps between days
 	cout << Tu << ThSp << sct << ThSp << MAT180 << endl;
 	cout << Tu << ThSp << tct << ThSp << CIS162 << endl;
 	cout << Tu << ThSp << foct << ThSp << PHY212 << endl;
 	cout << FSp << endl;
+	//Used for gaps between days
 	cout << WMD << ThSp << fct << ThSp << MAT216 << endl;
 	cout << WMD << ThSp << tct << ThSp << CIS162 << endl;
 	cout << WMD << ThSp << foct << ThSp << PHY212 << endl;
 	cout << FSp << endl;
+	//Used for gaps between days
 	cout << Th << ThSp << sct << ThSp << MAT180 << endl;
 	cout << Th << ThSp << tct << ThSp << CIS162 << endl;
 	cout << Th << ThSp << foct << ThSp << PHY212 << endl;
 	cout << FSp << endl;
+	//Used for gaps between days
 	cout << F << ThSp << tct << ThSp << CIS162 << endl;
 	cout << pt << endl;
-	/*
-	Part 2 Star chart
-	*/
+	//Header for part 2
 	cout << STAROne << endl;
 	cout << StarTwo << endl;
 	cout << STAROne << endl;
